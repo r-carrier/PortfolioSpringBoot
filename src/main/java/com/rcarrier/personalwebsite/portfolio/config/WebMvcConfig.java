9d415243d3c48a2e54778f38057c040408818472
r-carrier <robertjaycarrier@gmail.com>
2024-07-11 19:23:49 -0400

    Adding corsConfigurer for access to local Angular application

--- src/main/java/com/rcarrier/personalwebsite/portfolio/config/WebMvcConfig.java
+++ src/main/java/com/rcarrier/personalwebsite/portfolio/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.rcarrier.personalwebsite.portfolio.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,5 +14,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AppInterceptor());
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Adjust as needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
